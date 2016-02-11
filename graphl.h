18e05113ab42581ef6bf090f2de19d183df38060
Ngoc Luu <luubaongoc@live.com>
2016-02-11 01:04:20 -0800

    Update graphl.h

--- graphl.h
+++ graphl.h
@@ -1,21 +1,3 @@
-//------------------------------------------ graphl.h ---------------------------------------------
-// Programmer Name: Jeen Cherdchusilp & Ngoc Luu 
-// Creation Date: 10/26/2015 // Date of Last Modification: 02/06/2016
-// -------------------------------------------------------------------------------------------------
-// Purpose - a brief statement of the program's function 
-// This project aims to implement depth first search algorithm
-// ------------------------------------------------------------------------------------------------- 
-// Notes on specifications, special algorithms, and assumptions. 
-// -------------------------------------------------------------------------------------------------
-// GraphL class: 
-// This class aims to implement the depth first search algorithm.
-//
-// Implementation and assumptions:
-// -- A graph can have at most 100 nodes. (This assumption might change to the scale of the project)
-// -- This program assumes the input data has correctly formatted, valid data.
-// -- This program displays the result to demonstrate the algorithm works properly.
-//--------------------------------------------------------------------------------------------------
-
 #ifndef GRAPHL_H
 #define GRAPHL_H
 #include <string>	// for string
@@ -39,10 +21,12 @@ class GraphL
 		void depthFirstSearch();	// perform pre-order traversal in a graph
 		void displayGraph();		// display the result of depth first search
 		
-		//struct GraphNode;
 		void enumerateSubgraph(int); //enumerateSubgraph
-		void extendSubgraph(vector<int>, queue<int>, int, const int&); //extendSubgraph
-		void getExtension(const int&, queue<int>);
+		//void extendSubgraph(vector<int>, queue<int>, int, const int&); //extendSubgraph
+		//void getExtension(const int&, queue<int>&, vector<int>& Vsubgraph);
+		void extendSubgraph(vector<int>, vector<int>, int, const int&); //extendSubgraph
+		void getExtension(const int&, vector<int>&, vector<int>& Vsubgraph);
+		bool exclusiveNeighbor(int vertex, vector<int>& Vsubgraph);
 	
 	private:
 		struct EdgeNode;		 // forward reference for the compiler
